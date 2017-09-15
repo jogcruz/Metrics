a3ecb1602c068729352bd38c5fe62ded8c38be9f
Jorge Gomez <jorge.gomez@salesforce.com>
2017-09-15 11:44:11 -0400

    Working with Datatables

--- Metrics/bin/resources/js/app.js
+++ Metrics/bin/resources/js/app.js
@@ -25,7 +25,7 @@
             $('#div-table-tenant').show();
             if (oSettings.aoData.length > 0) {
                 var aData = oSettings.aoData[0]._aData;
-                tb_running.ajax.url('/api/tenants/1/running').load();
+                loadData(aData.id);
             }
         }
     });
@@ -54,9 +54,90 @@
 
     });
 
+    var tb_ended = $('#tb_ended').DataTable({
+        "info": false,
+        "searching": false,
+        "oLanguage": { "sSearch": "" },
+        "lengthChange": false,
+        "ajax": {
+            "dataSrc": ""
+        },
+        "columns": [
+            { "data": "name" },
+            { "data": "start" },
+            { "data": "end" },
+            { "data": "timeRunning" },
+            { "data": "timeAverage" }],
+
+        "drawCallback": function (settings) {
+            var pagination = $(this).closest('.dataTables_wrapper').find('.dataTables_paginate');
+            pagination.toggle(this.api().page.info().pages > 1);
+        },
+        "fnInitComplete": function (oSettings, json) {
+
+        }
+
+    });
+
+    var tb_failed = $('#tb_failed').DataTable({
+        "info": false,
+        "searching": false,
+        "oLanguage": { "sSearch": "" },
+        "lengthChange": false,
+        "ajax": {
+            "dataSrc": ""
+        },
+        "columns": [
+            { "data": "name" },
+            { "data": "start" },
+            { "data": "end" },
+            { "data": "timeRunning" },
+            { "data": "timeAverage" },
+            { "data": "reason" }],
+
+        "drawCallback": function (settings) {
+            var pagination = $(this).closest('.dataTables_wrapper').find('.dataTables_paginate');
+            pagination.toggle(this.api().page.info().pages > 1);
+        },
+        "fnInitComplete": function (oSettings, json) {
+
+        }
+
+    });
+
 
     $('.dataTable').on('click', 'tbody tr', function () {
-        console.log('API row values : ', tb_tenants.row(this).data());
+        //console.log('API row values : ', tb_tenants.row(this).data());
+        loadData(tb_tenants.row(this).data().id);
     })
+
+    function loadData(tenantId) {
+        $('#div-spinner-running').show();
+        $('#div-table-running').hide();
+        $('#div-spinner-ended').show();
+        $('#div-table-ended').hide();
+        $('#div-spinner-failed').show();
+        $('#div-table-failed').hide();
+
+
+        tb_running.ajax.url('/api/tenants/' + tenantId + '/running').load(function (data) {
+            // Hide spinner
+            $('#div-spinner-running').hide();
+            $('#div-table-running').show();
+        });
+
+        tb_ended.ajax.url('/api/tenants/' + tenantId + '/ended').load(function (data) {
+            // Hide spinner
+            $('#div-spinner-ended').hide();
+            $('#div-table-ended').show();
+        });
+
+        tb_failed.ajax.url('/api/tenants/' + tenantId + '/failed').load(function (data) {
+            // Hide spinner
+            $('#div-spinner-failed').hide();
+            $('#div-table-failed').show();
+        });
+    }
+
 
 }); 
